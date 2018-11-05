c93264b29f1c5afa5f5e59fefaf76e613fbd20c5
ricky han <github@rickyhan.com>
2018-11-05 17:39:29 -0500

    fix concavity

--- src/xprite/common/sorter.rs
+++ src/xprite/common/sorter.rs
@@ -1,17 +1,50 @@
 use xprite::*;
 use std::cmp::Ordering;
 
+pub fn get_concavity(path: &[Pixel]) -> bool {
+    let p1 = path[0];
+    let p2 = path[path.len() / 2];
+    let p3 = path[path.len()-1];
+
+    let x1 = p1.point.x;
+    let x2 = p2.point.x;
+    let x3 = p3.point.x;
+    let y1 = p1.point.y;
+    let y2 = p2.point.y;
+    let y3 = p3.point.y;
+
+    // assert!(x1 < x2);
+    // assert!(x2 < x3);
+
+    let m1 = (y2 as i32 - y1 as i32) / (x2 as i32 - x1 as i32);
+    let m2 = (y3 as i32 - y2 as i32) / (x3 as i32 - x2 as i32);
+
+    if m1 < m2 {
+        false
+    } else {
+        true
+    }
+}
+
 pub fn sort_path(path: &mut [Pixel]) -> Vec<Pixel> {
-    let mut dir = if path.iter().last().unwrap().point.y < path[0].point.y {
+
+    let going_up = path.iter().last().unwrap().point.y < path[0].point.y;
+    let mut dir = if going_up {
+        // console!(log, "going up");
         -1
     } else {
         1
     };
-    if path.iter().last().unwrap().point.x < path[0].point.x {
-        console!(log, "blah");
+    // if the path is drawn from right to left
+    let right_to_left = path.iter().last().unwrap().point.x < path[0].point.x;
+    if right_to_left {
+        // console!(log, "right to left");
         dir *= -1;
         path.reverse();
     };
+
+    let is_concave_up = get_concavity(path);
+    // console!(log, format!("concavity: {}", is_concave_up));
 
     let mut segs = Vec::new();
     let mut p0 = path[0];
@@ -48,35 +81,41 @@ pub fn sort_path(path: &mut [Pixel]) -> Vec<Pixel> {
         else {Ordering::Greater}
     });
 
+    if (is_concave_up && !going_up)
+    || (!is_concave_up && going_up) {
+        segs.reverse();
+    }
+
+    if right_to_left {
+        segs.reverse();
+    }
+
     let mut ret = Vec::new();
     let mut p0 = path[0];
-
     p0.point.y -= 1;
     p0.point.x -= 1;
     for &((dx, dy), _) in segs.iter() {
-        let mut dx = dx;
-        let mut dy = dy;
-            if dx == 1 {
-                p0.point.x += 1;
-                for _ in 0..dy {
-                    if dir == 1 {
-                        p0.point.y += 1;
-                    } else {
-                        p0.point.y -= 1;
-                    }
-                    ret.push(p0);
-                }
-            } else if dy == 1 {
+        if dx == 1 {
+            p0.point.x += 1;
+            for _ in 0..dy {
                 if dir == 1 {
                     p0.point.y += 1;
                 } else {
                     p0.point.y -= 1;
                 }
-                for _ in 0..dx {
-                    p0.point.x += 1;
-                    ret.push(p0);
-                }
+                ret.push(p0);
             }
+        } else if dy == 1 {
+            if dir == 1 {
+                p0.point.y += 1;
+            } else {
+                p0.point.y -= 1;
+            }
+            for _ in 0..dx {
+                p0.point.x += 1;
+                ret.push(p0);
+            }
+        }
     }
 
     // console!(log, format!("{:#?}", segs));
