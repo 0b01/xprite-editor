69de0cf7e152b247f8e314b9a5eb6f9649594ed8
Ricky Han <rickylqhan@gmail.com>
2018-12-16 18:21:46 -0500

    scripting interop

--- xprite/src/scripting/mod.rs
+++ xprite/src/scripting/mod.rs
@@ -1,18 +1,17 @@
 use crate::prelude::*;
 
-use dyon::{load, Dfn, Module, Runtime, Vec4};
-use std::sync::{Arc};
+use dyon::{
+    load, Dfn, Module, Runtime, Vec4,
+    embed::PopVariable
+};
+use std::sync::{Arc, Mutex};
 
 pub struct Scripting {
-    runtime: dyon::Runtime,
 }
 
 impl Scripting {
     pub fn new() -> Self {
-        let runtime = Runtime::new();
-        Self {
-            runtime,
-        }
+        Self{}
     }
 
     pub fn execute(&mut self, xpr: &mut Xprite) -> Option<()> {
@@ -20,31 +19,40 @@ impl Scripting {
         use dyon::{Type, Lt};
 
         let mut module = Module::new();
-        module.add(Arc::new("say_hello".to_owned()), say_hello, Dfn{ lts: vec![], tys:vec![], ret: Type::Void});
-        module.add(Arc::new("pixel".to_owned()), pixel, Dfn{
-            lts: vec![Lt::Default, Lt::Default],
-            tys:vec![Type::Vec4, Type::Vec4],
-            ret: Type::Void
+        let ty_xpr = Type::AdHoc(Arc::new("XprState".into()), Box::new(Type::Any));
+        module.add(Arc::new("xpr_new".into()), new_xpr_state, Dfn{
+            lts: vec![],
+            tys: vec![],
+            ret: ty_xpr.clone()
         });
-
         match load("scripts/main.dyon", &mut module) {
             Err(msg) => { error!("{}", msg); return Some(()); }
             _ => (),
         };
 
-        match self.runtime.run(&Arc::new(module)) {
+        let mut runtime = Runtime::new();
+        match runtime.call_str_ret("render", &Vec::new(), &Arc::new(module)) {
+            Ok(msg) => {
+                let i = XprState::pop_var(&runtime, &msg).unwrap();
+                info!("{:#?}", i);
+            }
             Err(msg) => { error!("{}", msg); return Some(()); }
-            _ => (),
         };
         Some(())
     }
 }
 
-dyon_fn!{fn pixel(v: Vec4, col: Vec4) {
-    println!("{:?} {:?}", v, col);
-}}
+#[derive(Debug)]
+struct XprState {
+    to_draw: Vec<(Vec4, Vec4)>,
+}
 
+dyon_obj! {
+    XprState {
+        to_draw
+    }
+}
 
-dyon_fn!{fn say_hello() {
-    println!("hi!");
+dyon_fn!{fn new_xpr_state() -> XprState {
+    XprState{to_draw: vec![]}
 }}
